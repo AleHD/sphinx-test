3cef2fe5ec12523fbe2613661cfa56402799f2f2
AleHC <alejandro.hernandezcano@epfl.ch>
2023-08-07 17:10:45 +0200

    8

--- docs/conf.py
+++ docs/conf.py
@@ -9,10 +9,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.pardir)
-
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
